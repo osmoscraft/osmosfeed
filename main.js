965f10f8723f00299668516ff36940963c3a3234
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-11-14 16:04:37 -0800

    output a list of articles

--- main.js
+++ main.js
@@ -7,20 +7,39 @@ import axios from "axios";
 async function run() {
   const sourcesText = fs.readFileSync(path.resolve("sources.yaml"), "utf8");
 
+  /** @type {string[]} */
   const sources = yaml.safeLoad(sourcesText);
 
   const pagesAsync = sources.map(async (source) => {
     const response = await axios.get(source.href);
     const xmlString = response.data;
     const feed = htmlparse2.parseFeed(xmlString);
-    return feed;
+
+    const items = feed.items ?? [];
+
+    return items.map((item) => {
+      const { title, link, pubDate } = item;
+
+      return {
+        sourceHref: source.href,
+        title,
+        link,
+        publishedOn: pubDate?.toISOString(),
+      };
+    });
   });
 
-  const results = await Promise.all(pagesAsync);
+  const results = (await Promise.all(pagesAsync)).flat();
+  console.log(results);
 
-  results.map((result) => {
-    console.log(result);
-  });
+  const outputHtml = results
+    .map(
+      (result) =>
+        `<article><a href="${result.link}">${result.title}<a></article>`
+    )
+    .join("\n");
+
+  fs.writeFileSync(path.resolve("output.html"), outputHtml);
 }
 
 run();
