73cc737b5c7c9fbd091372939e5d306931c6ee5b
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2021-02-24 18:11:02 -0800

    switch to esbuild

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,15 @@
 const path = require("path");
 const fs = require("fs-extra");
-const ncc = require("@vercel/ncc");
 
-ncc(path.resolve("src/main.ts")).then(({ code }) => {
-  const outputPath = path.resolve("bin/main.js");
-  fs.ensureFileSync(outputPath);
-  fs.writeFileSync(outputPath, code);
+require("esbuild")
+  .build({
+    platform: "node",
+    entryPoints: ["src/main.ts"],
+    bundle: true,
+    format: "cjs",
+    outfile: "bin/main.js",
+  })
+  .catch(() => process.exit(1));
 
-  fs.copySync(path.resolve("src/index-template.html"), path.resolve("bin/index-template.html"));
-  fs.copySync(path.resolve("src/assets"), path.resolve("bin/assets"));
-});
+fs.copySync(path.resolve("src/index-template.html"), path.resolve("bin/index-template.html"));
+fs.copySync(path.resolve("src/assets"), path.resolve("bin/assets"));
