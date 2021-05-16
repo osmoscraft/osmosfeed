de07f9a6459555f54041a8d223aa3843b6428672
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2021-05-15 19:11:39 -0700

    fixed: static files were unnecessarily copied for a custom template

--- src/lib/copy-static.ts
+++ src/lib/copy-static.ts
@@ -1,20 +1,32 @@
 import fs from "fs-extra";
 import path from "path";
 import { ENTRY_DIR } from "../utils/entry-dir";
+import type { TemplateSummary } from "./get-templates";
 
 export const USER_STATIC_DIR = "static";
 export const SYSTEM_STATIC_DIR = "system-static";
 export const PUBLIC_ROOT_DIR = "public";
 
-export async function copyStatic() {
-  await copySystemStatic();
+const FAVICON_FILENAME = "favicon.ico";
+
+export async function copyStatic(templateSummary: TemplateSummary) {
+  const isSystemTemplateIntact = templateSummary.partials.every((partials) => partials.source === "system");
+  await copySystemStatic(isSystemTemplateIntact);
+
   // copy user asset last to allow overwrite
   await copyUserStatic();
 }
 
-export async function copySystemStatic() {
-  await fs.copy(path.resolve(ENTRY_DIR, SYSTEM_STATIC_DIR), path.resolve(PUBLIC_ROOT_DIR));
-  console.log(`[copy] System static files copied`);
+export async function copySystemStatic(isSystemTemplateIntact: boolean) {
+  const systemStaticDir = path.resolve(ENTRY_DIR, SYSTEM_STATIC_DIR);
+  // To ensure assets compatibility, copy system static files only when all the templates are from the system.
+  if (isSystemTemplateIntact) {
+    await fs.copy(systemStaticDir, path.resolve(PUBLIC_ROOT_DIR));
+    console.log(`[copy] System static files copied`);
+  } else {
+    await fs.copy(path.join(systemStaticDir, FAVICON_FILENAME), path.resolve(PUBLIC_ROOT_DIR, FAVICON_FILENAME));
+    console.log(`[copy] User template present, only ${FAVICON_FILENAME} was copied`);
+  }
 }
 
 export async function copyUserStatic() {
--- src/lib/get-templates.ts
+++ src/lib/get-templates.ts
@@ -5,34 +5,38 @@ import { ENTRY_DIR } from "../utils/entry-dir";
 export const INCLUDE_DIR = "includes";
 export const SYSTEM_TEMPLATE_DIR = "system-templates";
 
-export interface UserTemplateSummary {
+export interface TemplateSummary {
   partials: Partial[];
 }
 
 export interface Partial {
   name: string;
   template: string;
+  source: PartialSource;
 }
+
+export type PartialSource = "user" | "system";
 
 interface FileSummary {
   path: string;
   ext: string;
   filename: string;
+  source: PartialSource;
 }
 
 /**
  * Detect and load templates.
  * User templates from "include" overwrites system templates from "src/system-templates".
  */
-export async function getTemplates(): Promise<UserTemplateSummary> {
+export async function getTemplates(): Promise<TemplateSummary> {
   const systemIncludeDir = path.resolve(ENTRY_DIR, SYSTEM_TEMPLATE_DIR);
   const systemIncludeFilenames = await fs.readdir(systemIncludeDir).catch(() => [] as string[]);
 
   const userIncludeDir = path.resolve(INCLUDE_DIR);
   const userIncludeFilenames = await fs.readdir(userIncludeDir).catch(() => [] as string[]);
 
-  const userIncludeFileMapper = getFileSummaryMapper(userIncludeDir);
-  const systemIncludeFileMapper = getFileSummaryMapper(systemIncludeDir);
+  const userIncludeFileMapper = getFileSummaryMapper(userIncludeDir, "user");
+  const systemIncludeFileMapper = getFileSummaryMapper(systemIncludeDir, "system");
 
   const includeFileDetails = [
     ...userIncludeFilenames.map(userIncludeFileMapper),
@@ -52,10 +56,11 @@ export async function getTemplates(): Promise<UserTemplateSummary> {
       fs
         .readFile(htmlInclude.path, "utf-8")
         .then((content) => {
-          console.log(`[template] Loaded ${htmlInclude.path}`);
+          console.log(`[template] Loaded ${htmlInclude.source} template: ${htmlInclude.path}`);
           return {
             name: htmlInclude.filename.replace(".hbs", ""),
             template: content,
+            source: htmlInclude.source,
           };
         })
         .catch((e) => {
@@ -72,10 +77,11 @@ export async function getTemplates(): Promise<UserTemplateSummary> {
   };
 }
 
-function getFileSummaryMapper(dir: string) {
+function getFileSummaryMapper(dir: string, source: PartialSource) {
   return (filename: string) => ({
     path: path.join(dir, filename),
     ext: path.extname(filename).toLowerCase(),
     filename: filename,
+    source,
   });
 }
