c73b08a9053412f10224cdfa22cff7b257efa1cd
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2021-05-20 21:32:26 -0700

    reuse public path from constant

--- packages/cli/src/lib/render-files.ts
+++ packages/cli/src/lib/render-files.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs-extra";
 import { FEED_FILENAME, INDEX_FILENAME } from "./render-atom";
+import { PUBLIC_ROOT_DIR } from "./copy-static";
 
 export interface RenderFilesInput {
   html: string;
@@ -10,9 +11,9 @@ export interface RenderFilesInput {
 export async function renderFiles(input: RenderFilesInput) {
   const { html, atom } = input;
 
-  await fs.ensureDir(path.resolve("public")).then(() => {
-    const indexPath = path.resolve(`public/${INDEX_FILENAME}`);
-    const atomPath = path.resolve(`public/${FEED_FILENAME}`);
+  await fs.ensureDir(path.resolve(PUBLIC_ROOT_DIR)).then(() => {
+    const indexPath = path.resolve(`${PUBLIC_ROOT_DIR}/${INDEX_FILENAME}`);
+    const atomPath = path.resolve(`${PUBLIC_ROOT_DIR}/${FEED_FILENAME}`);
 
     const renderHtmlAsync = fs.writeFile(indexPath, html).then(() => console.log(`[render] Rendered: ${indexPath}`));
     const renderAtomAsync = fs.writeFile(atomPath, atom).then(() => console.log(`[render] Rendered: ${atomPath}`));
