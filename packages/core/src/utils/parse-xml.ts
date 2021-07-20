9d711980025cafc137a1a7fd84d909b17abe0bb3
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2021-07-19 23:03:15 -0700

    update todo

--- packages/core/src/utils/parse-xml.ts
+++ packages/core/src/utils/parse-xml.ts
@@ -1,6 +1,7 @@
 import { create } from "xmlbuilder2";
 
 export function parseXml(input: string) {
+  // TODO switch to htmlparser2 for lower level control and speed
   const doc = create(input);
-  return doc.end({format: "object"});
+  return doc.end({ format: "object" });
 }
