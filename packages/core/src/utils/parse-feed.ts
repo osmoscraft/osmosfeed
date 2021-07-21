0dc80b4cca9ebbe63bab236811d0a7c7728fb189
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2021-07-21 07:31:54 -0700

    remove converter factory

--- packages/core/src/utils/parse-feed.ts
+++ packages/core/src/utils/parse-feed.ts
@@ -1,10 +1,22 @@
+import cheerio from "cheerio";
 import htmlparser2 from "htmlparser2";
-import { getConverter } from "./get-converter.js";
+import { RssToJsonFeedConverter } from "./json-feed-converters.js";
+
+export class UnknownFeedTypeError extends Error {}
 
 export function xmlToJsonFeed(input: string) {
   const dom = htmlparser2.parseDocument(input, { xmlMode: true, decodeEntities: true });
 
-  const converter = getConverter(dom);
+  const cheerioDom = cheerio.load(dom, { xmlMode: true, decodeEntities: false });
+
+  let converter;
+
+  // TODO refactor into a function
+  if (cheerioDom("rss").length) {
+    converter = new RssToJsonFeedConverter(cheerioDom);
+  } else {
+    throw new UnknownFeedTypeError();
+  }
 
   return converter.convert();
 }
