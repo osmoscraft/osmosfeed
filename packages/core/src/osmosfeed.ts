3272c297b3bcd1776eccda1bb333e022aa7f5539
Chuanqi Sun <cs@pop-os.localdomain>
2021-08-01 19:27:50 -0700

    fix build error

--- packages/core/src/osmosfeed.ts
+++ packages/core/src/osmosfeed.ts
@@ -1,12 +1,12 @@
 import { from, map, mergeMap } from "rxjs";
 import { fromHttpGet } from "./utils/from-http-get.js";
-import { parseXml } from "./utils/parse-xml.js";
+import { parseFeed } from "./utils/parse-feed.js";
 
 export function osmosfeed(sources: string[]) {
   from(sources).pipe(
     mergeMap(fromHttpGet),
     map(response => response?.raw),
-    map(parseXml)
+    map(parseFeed)
   )
     .subscribe(result => console.log(result))
 }
