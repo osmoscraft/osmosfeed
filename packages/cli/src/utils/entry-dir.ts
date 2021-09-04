a9d3a12f212b9f00bcafa517e555c87847c350b4
Chuanqi Sun <cs@pop-os.localdomain>
2021-09-03 17:17:20 -0700

    remove v1 packages

--- packages/cli/src/utils/entry-dir.ts
+++
@@ -1,4 +0,0 @@
-import path from "path";
-
-/** The dir that contains of index.ts/js */
-export const ENTRY_DIR = path.dirname(require?.main?.filename ?? __dirname);
--- packages/cli/src/utils/trim-with-threshold.ts
+++
@@ -1,3 +0,0 @@
-export function trimWithThreshold(input: string, activationThreshold: number, trimTo: number): string {
-  return input.length > activationThreshold ? `${input.slice(0, trimTo)}…` : input;
-}
