ecb01de29df2cc1c3b31f3f2fbdd8def1c533e93
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2021-09-26 12:13:34 -0700

    library update

--- packages/cli/src/lib/__tests__/normalize-url.test.ts
+++ packages/cli/src/lib/__tests__/normalize-url.test.ts
@@ -3,21 +3,23 @@ import { normalizeUrl } from "../normalize-url";
 
 describe("normalizeUrl", () => {
   it("handles url that doesn't need encoding", async () => {
-    expect(normalizeUrl("https://www.mockdomain.com")).toEqual("https://www.mockdomain.com");
+    await expect(normalizeUrl("https://www.mockdomain.com")).toEqual("https://www.mockdomain.com");
   });
 
   it("handles url that needs encoding", async () => {
-    expect(normalizeUrl("https://www.mockdomain.com/hello world")).toEqual("https://www.mockdomain.com/hello%20world");
+    await expect(normalizeUrl("https://www.mockdomain.com/hello world")).toEqual(
+      "https://www.mockdomain.com/hello%20world"
+    );
   });
 
   it("handles url that is encoded", async () => {
-    expect(normalizeUrl("https://www.mockdomain.com/hello%20world")).toEqual(
+    await expect(normalizeUrl("https://www.mockdomain.com/hello%20world")).toEqual(
       "https://www.mockdomain.com/hello%20world"
     );
   });
 
   it("handles url unicode characters", async () => {
-    expect(normalizeUrl("https://www.你好.com?query=世界")).toEqual(
+    await expect(normalizeUrl("https://www.你好.com?query=世界")).toEqual(
       "https://www.%E4%BD%A0%E5%A5%BD.com?query=%E4%B8%96%E7%95%8C"
     );
   });
