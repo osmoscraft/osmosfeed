ad680bfb96016f8a07151b933b0b066ec6e6c6c1
sora <chuanqi.sun.dev@gmail.com>
2021-10-16 21:44:12 -0700

    debug multiple react copies

--- packages/builder/vite.config.ts
+++ packages/builder/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // This config is used for building the server, which at runtime, will build the client
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ["fs/promises", "vite"],
