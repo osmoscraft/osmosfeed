c5f9c01bb17972bb7431150a76f8536007bf3cd7
sora <chuanqi.sun.dev@gmail.com>
2022-02-13 21:12:17 -0800

    udpate test

--- packages/cli/src/lib/__tests__/time.test.ts
+++ packages/cli/src/lib/__tests__/time.test.ts
@@ -4,10 +4,11 @@ import { getOffsetFromTimezoneName } from "../time";
 describe("getOffsetFromTimezoneName", () => {
   it("handles url that doesn't need encoding", () => {
     // Note that the symbol is inverted from international convention
-    expect(getOffsetFromTimezoneName("Asia/Shanghai")).toBe(-480); // +8
-    expect(getOffsetFromTimezoneName("Asia/Tokyo")).toBe(-540); // +9
+    // Source: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    expect(getOffsetFromTimezoneName("Asia/Shanghai")).toBe(-480); // GMT+8
+    expect(getOffsetFromTimezoneName("Asia/Tokyo")).toBe(-540); // GMT+9
     expect(getOffsetFromTimezoneName("UTC")).toBe(0);
-    expect(getOffsetFromTimezoneName("EST")).toBe(300);
-    expect(getOffsetFromTimezoneName("Pacific/Honolulu")).toBe(600);
+    expect(getOffsetFromTimezoneName("EST")).toBe(300); // GMT-5
+    expect(getOffsetFromTimezoneName("Pacific/Honolulu")).toBe(600); //GMT-10
   });
 });
