a6f5b5083cec91edf6bab926b4eefa1af775bd41
sora <chuanqi.sun.dev@gmail.com>
2022-06-20 22:31:11 -0700

    reorg content

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,10 @@
-import { useCacheReader, useCacheWriter } from "./mods/cache/cache";
-import { useConfig } from "./mods/config/config";
-import { useDownload } from "./mods/download/download";
-import { useMerge } from "./mods/merge/merge";
-import { useNormalize } from "./mods/normalize/normalize";
-import { useParse } from "./mods/parse/parse";
-import type { PipeFeed } from "./mods/types";
+import { useCacheReader, useCacheWriter } from "./modules/cache/cache";
+import { useConfig } from "./modules/config/config";
+import { useDownload } from "./modules/download/download";
+import { useMerge } from "./modules/merge/merge";
+import { useNormalize } from "./modules/normalize/normalize";
+import { useParse } from "./modules/parse/parse";
+import type { PipeFeed } from "./modules/types";
 import { asyncVectorPipeFactory } from "./utils/async-vector-pipe";
 
 const pipe = asyncVectorPipeFactory<PipeFeed>(
