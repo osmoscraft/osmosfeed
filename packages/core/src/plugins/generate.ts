c9b2de8bf945991881f261e0df84cea9d8250d90
sora <chuanqi.sun.dev@gmail.com>
2022-06-26 11:11:33 -0700

    resolve user files

--- packages/core/src/plugins/generate.ts
+++ packages/core/src/plugins/generate.ts
@@ -1,32 +1,50 @@
 import { readFile, writeFile } from "fs/promises";
 import Handlebars from "handlebars";
-import { basename, extname, join } from "path";
+import { basename, extname, isAbsolute, join, relative } from "path";
 import type { ProjectTask } from "../engine/build";
 import { readdirDeep } from "./generate/fs-helper";
 import { getAtomXml } from "./generate/get-atom-xml";
 import { getTemplateData } from "./generate/get-template-data";
 import type { Project } from "./types";
 
-const ENTRY_TEMPLATE_NAME = "index";
+const ENTRY_BASENAME = "index";
 export const ATOM_FILENAME = "feed.atom";
 export const INDEX_FILENAME = "index.html";
 
 export function generate(): ProjectTask<Project> {
   return async (project) => {
-    const { templateFiles: sysTempaltes, staticFiles: sysStatics } = await discoverAssets(
-      join(__dirname, "generate/assets")
+    const defaultDir = join(__dirname, "generate/default");
+    const userDir = process.cwd();
+
+    const [defaultTemplates, defaultStatic, userTemplates, userStatic] = await Promise.all([
+      preloadDir(join(defaultDir, "templates"), templateFilter),
+      preloadDir(join(defaultDir, "static")),
+      preloadDir(join(userDir, "templates"), templateFilter),
+      preloadDir(join(userDir, "static")),
+    ]);
+
+    const templates = mergeAssets(userTemplates, defaultTemplates);
+    const statics = mergeAssets(userStatic, defaultStatic);
+
+    console.log(
+      `[generate] Active default files`,
+      [...templates, ...statics].filter((f) => isParentChild(defaultDir, f.path)).map((t) => t.path)
     );
-    const { templateFiles: userTempaltes, staticFiles: userStatics } = await discoverAssets(process.cwd());
+
+    console.log(
+      `[generate] Active user files`,
+      [...templates, ...statics].filter((f) => isParentChild(userDir, f.path)).map((t) => t.path)
+    );
 
     await Promise.all(
-      sysTempaltes.map(async (templateFile) => {
-        const template = await templateFile;
+      templates.map(async (templateFile) => {
+        const template = templateFile;
         const text = await template.readAsync();
         Handlebars.registerPartial(template.baseName, text);
       })
     );
 
-    const execTemplate = Handlebars.compile(`{{> ${ENTRY_TEMPLATE_NAME}}}`);
+    const execTemplate = Handlebars.compile(`{{> ${ENTRY_BASENAME}}}`);
     const templateData = getTemplateData(project);
     const htmlString = execTemplate(templateData);
 
@@ -43,43 +61,54 @@ export function generate(): ProjectTask<Project> {
   };
 }
 
-async function discoverAssets(assetsDir: string) {
-  const templateDir = join(assetsDir, "templates");
-  const templatePaths = (await readdirDeep(templateDir)).filter((file) => extname(file) === ".hbs");
-  const templateFiles = Promise.all(templatePaths.map((file) => join(templateDir, file)).map(preload));
-
-  const staticDir = join(assetsDir, "static");
-  const staticPaths = await readdirDeep(staticDir);
-  const staticFiles = Promise.all(staticPaths.map((file) => join(staticDir, file)).map(preload));
-
-  const [templatePreloads, staticPreloads] = await Promise.all([templateFiles, staticFiles]);
-
-  return {
-    templateFiles: templatePreloads,
-    staticFiles: staticPreloads,
-  };
+function templateFilter(file: PreloadedFile) {
+  const TEMPLATE_EXT = [".hbs", ".html", ".htm"];
+  return TEMPLATE_EXT.includes(file.extName);
 }
 
 interface PreloadedFile {
   path: string;
+  relPath: string;
   baseName: string;
   extName: string;
   name: string;
   readAsync: () => Promise<string>;
 }
 
-async function preloadDir(dir: string, filter?: (file: PreloadedFile) => boolean): Promise<PreloadedFile[]> {
-  return (await readdirDeep(dir)).map(preload).filter(filter ?? (() => true));
+function mergeAssets(userFiles: PreloadedFile[], sysFiles: PreloadedFile[]): PreloadedFile[] {
+  return [...userFiles, ...sysFiles].filter(
+    (file, index, existingFiles) =>
+      index === existingFiles.findIndex((existingFile) => existingFile.relPath === file.relPath)
+  );
 }
 
-function preload(path: string): PreloadedFile {
-  const ext = extname(path);
+async function preloadDir(dir: string, filter?: (file: PreloadedFile) => boolean): Promise<PreloadedFile[]> {
+  try {
+    return (await readdirDeep(dir)).map((path) => preload(path, dir)).filter(filter ?? (() => true));
+  } catch (e) {
+    if ((e as any)?.code === "ENOENT") {
+      return [];
+    } else {
+      throw e;
+    }
+  }
+}
+
+function preload(relPath: string, baseDir: string): PreloadedFile {
+  const ext = extname(relPath);
+  const longPath = join(baseDir, relPath);
 
   return {
-    path,
-    name: basename(path),
-    baseName: basename(path, ext),
+    path: longPath,
+    relPath: relPath,
+    name: basename(longPath),
+    baseName: basename(longPath, ext),
     extName: ext,
-    readAsync: () => readFile(path, "utf-8"),
+    readAsync: () => readFile(longPath, "utf-8"),
   };
+}
+
+function isParentChild(parent: string, child: string) {
+  const relativePath = relative(parent, child);
+  return relativePath && !relativePath.startsWith("..") && !isAbsolute(relativePath);
 }
