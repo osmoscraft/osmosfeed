4780344e5b783510f175e3dcd72c4be4eababbde
sora <chuanqi.sun.dev@gmail.com>
2022-06-26 17:51:53 -0700

    update cli

--- packages/cli/main.ts
+++ packages/cli/main.ts
@@ -1,9 +1,19 @@
 #!/usr/bin/env tsx
 import { build, cache, configFileYaml, download, generate, merge, normalize, parse, prune } from "@osmosfeed/core";
 
+console.log(`[cli] starting build`);
+
 build({
   preProjectTasks: [configFileYaml()],
   preFeedTasks: [download(), parse()],
   postFeedTasks: [normalize(), merge(), cache()],
   postProjectTasks: [generate(), prune()],
-});
+})
+  .then(() => {
+    console.log("[cli] build success");
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error("[cli] build failed", e);
+    process.exit(1);
+  });
