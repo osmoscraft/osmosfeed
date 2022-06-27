bfd6fa7ae78dfc6da78dfbdd939f13c87e9deaea
sora <chuanqi.sun.dev@gmail.com>
2022-06-26 17:08:50 -0700

    create cli package

--- packages/cli/main.ts
+++ packages/cli/main.ts
@@ -1,2 +1,9 @@
 #!/usr/bin/env tsx
-console.log("Hello World");
+import { build, cache, configFileYaml, download, generate, merge, normalize, parse, prune } from "@osmosfeed/core";
+
+build({
+  preProjectTasks: [configFileYaml()],
+  preFeedTasks: [download(), parse()],
+  postFeedTasks: [normalize(), merge(), cache()],
+  postProjectTasks: [generate(), prune()],
+});
